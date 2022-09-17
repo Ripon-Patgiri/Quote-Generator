5629c69d6661f7e64cc2a35589e5e8dbce9367ce
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-09-17 11:14:11 +0530

    Created Quotes Array.

--- script.js
+++ script.js
@@ -3,3 +3,48 @@
 let btn = document.querySelector("#new-quote");
 let quote = document.querySelector(".quote");
 let person = document.querySelector(".person");
+
+const quotes = [
+  {
+    quote: `“When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love…” `,
+    person: `Marcus Aurelius`
+  },
+  {
+    quote: `“Man is not worried by real problems so much as by his imagined anxieties about real problems.” `,
+    person: `Epictetus`
+  }
+  {
+    quote: `“It’s not because things are difficult that we dare not venture. It’s because we dare not venture that they are difficult.” `,
+    person: `Seneca`
+  }
+  {
+    quote: `“Do not indulge in dreams of having what you have not, but reckon up the chief of the blessings you do possess, and then thankfully remember how you would crave for them if they were not yours.” 
+
+    `,
+    person: `Marcus Aurelius`
+  }
+  {
+    quote: `“You have to grow from the inside out. None can teach you, none can make you spiritual. There is no other teacher but your own soul.”`,
+    person: `Swami Vivekananda`
+  }
+  {
+    quote: `“If you cry because the sun has gone out of your life, your tears will prevent you from seeing the stars.”`,
+    person: `Rabindranath Tagore`
+  }
+  {
+    quote: `“One is never afraid of the unknown; one is afraid of the known coming to an end.”`,
+    person: `Jiddu Krishnamurti`
+  }
+  {
+    quote: `“Same thing sometimes gives delight, sometimes pain, sometimes creates jealousy and sometimes anger. Therefore everything in this world is a source of misery.” `,
+    person: `Vishnu Purana`
+  }
+  {
+    quote: `“Set your Heart upon your Work, but never on its reward.” `,
+    person: `Bhagavad Gita`
+  }
+  {
+    quote: `“Be active, never be inactive, and don't react to the outcome of the work.” `,
+    person: `Shri Krishna`
+  }
+];
