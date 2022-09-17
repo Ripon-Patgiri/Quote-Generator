dd8fbe29e2d55a66c6973e02e5d26786d07e0627
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-09-17 10:49:36 +0530

    Linked Script and Extracted Necessar DOM Elements.

---
+++ script.js
@@ -0,0 +1,5 @@
+// Storing Necessary DOM Elements in Variables.
+
+let btn = document.querySelector("#new-quote");
+let quote = document.querySelector(".quote");
+let person = document.querySelector(".person");
